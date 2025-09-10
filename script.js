1117a83a9c811c970719ab9dcf0b3b6a2b029acd
eduarda <maria.oikawa@etec.sp.gov.br>
2025-09-10 14:57:21 -0300

    SOBEEEEE

---
+++ script.js
@@ -0,0 +1,8 @@
+const recyclingPoints = [
+  { id:1, name:"Eco Ponto Sorocaba - Parque das Águas", address:"Av. Dom Aguirre, 1000 - Sorocaba, SP", lat:-23.4905, lon:-47.4547 },
+  { id:2, name:"Eco Ponto Campinas - Taquaral", address:"Av. Heitor Penteado, 2145 - Campinas, SP", lat:-22.8897, lon:-47.0481 },
+  { id:3, name:"Eco Ponto São Paulo - Vila Mariana", address:"Rua Vergueiro, 1000 - São Paulo, SP", lat:-23.5880, lon:-46.6330 },
+  { id:4, name:"Eco Ponto Santos - Centro", address:"Av. Conselheiro Nébias, 200 - Santos, SP", lat:-23.9618, lon:-46.3280 },
+  { id:5, name:"Eco Ponto São José dos Campos - Centro", address:"Rua XV de Novembro, 400 - São José dos Campos, SP", lat:-23.1896, lon:-45.8841 },
+  { id:6, name:"Eco Ponto Ribeirão Preto - Centro", address:"Rua Álvares Cabral, 50 - Ribeirão Preto, SP", lat:-21.1785, lon:-47.8060 }
+];
